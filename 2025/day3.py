d05aa546970016b4411efc118d6257fbf31035f7
Sophie <gt>
2025-12-08 19:42:32 +0000

    mend (made a big mess on git when trying to amend previous commit message)

--- 2025/day3.py
+++ 2025/day3.py
@@ -7,17 +7,45 @@ https://adventofcode.com/2025/day/3
 
 # PART 1
 
+banks = []
 total_joltage = 0
+
+def max_joltage(bank: list, n: int):
+    # digits = []
+    max_jolt = ""
+    marker = 0
+    for i in range(n, 0, -1):
+        print(i)
+        end_marker = (-i+1 if i>1 else None)
+        try:
+            digit = max(bank[marker:end_marker])
+        except:
+            break
+        max_jolt += str(digit)
+        marker = bank.index(digit) + 1
+        print(f"digit: {digit}, marker: {marker}, max jolt: {max_jolt}")
+    # max_jolt = [str(digit) for digit in digits]
+    # total_joltage += int(max_jolt)
+    # print(max_jolt)
+    return max_jolt
 
 while True:
     try:
         bank = [int(i) for i in input().strip()]
-        max_jolt = ""
-        first_digit = max(bank[:-1])
-        second_digit = max(bank[bank.index(first_digit)+1:])
-        max_jolt = str(first_digit) + str(second_digit)
-        total_joltage += int(max_jolt)
-    except:
+        # banks.append(bank)
+        total_joltage += int(max_joltage(bank, 2))
+    except Exception as e:
         break
 
 print(f"pt1 - total output joltage: {total_joltage}")
+
+
+# PART 2
+
+total_joltage = 0
+
+for bank in banks:
+    print(bank)
+    total_joltage += int(max_joltage(bank, 12))
+
+print(f"pt2 - new total output joltage: {total_joltage}")
