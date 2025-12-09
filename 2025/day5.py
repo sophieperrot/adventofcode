a7ba5a36a348f806c4e811de7ce9387cd78168ac
Sophie <gt>
2025-12-09 12:37:03 +0000

    Revert "mend" (the mistake)
    This reverts commit aaa6351c68638756740f06b220b532670d884e96.

--- 2025/day5.py
+++ 2025/day5.py
@@ -7,35 +7,28 @@ https://adventofcode.com/2025/day/5
 
 # PART 1
 
-fresh_ranges = {}
+fresh_ids = set()
 
 id_range = input()
 while True:
     try:
-        start, end = id_range.split("-")
-        fresh_ranges[int(start)] = int(end)+1
-        # print(fresh_ids)
+        start = int(id_range.split("-")[0])
+        end = int(id_range.split("-")[1]) + 1
+        for i in range(start, end):
+            fresh_ids.add(i)
         id_range = input()
     except Exception as e:
         # print(e)
         break
 
-fresh_ids = set()
 num_fresh = 0
 while True:
-    # print("yay2")
     try:
         id = int(input())
-        # print(id)
-        for i, start in enumerate(fresh_ranges):
-            # print("yay")
-            end = fresh_ranges[start]
-            if id in range(start, end):
-                # print("fresh", id)
-                fresh_ids.add(id)
-                # num_fresh += 1
+        if id in fresh_ids:
+            num_fresh += 1
     except Exception as e:
         # print(e)
         break
 
-print(f"pt1 - number of fresh IDs: {len(fresh_ids)}")
+print(f"pt1 - number of fresh IDs: {num_fresh}")
